3699290235bf353674e19ecc64467fcd86a4e3dd
unknown <tyaps002@gmail,com>
2022-11-04 13:05:37 +0100

    Create comment and likes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     def current_user
-        @users.first
+        User.find(params[:user_id])
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user
+    @post = current_user.posts.new(post_params[:id])
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_post_path(current_user, @post), notice: 'Post was successfully created.'
+          # redirect_to '/users/#{@post_new.user_id}/posts', messaage: 'Post was successfully created.'
+        else
+          render :new, status: 'Error: Post was not created.'
+        end
+      end
+    end
   end
 end
