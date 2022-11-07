86fa184b7bf751edaf073061232fb31035816efb
vanessaBonito <vanebonito@hotmail.com>
2022-11-07 15:05:17 -0500

    Add test to see user name for user show view

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe 'users/show.html.erb', type: :view do
     expect(page).to have_css("img[src='#{@user.photo}']")
   end
 
+  it 'I can see the user name' do
+    expect(page).to have_content(@user.name)
+  end
+
 end
