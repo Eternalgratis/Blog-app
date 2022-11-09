9b703ce3885cc24acd876b8f11de04d2f8e020be
unknown <tyaps002@gmail,com>
2022-11-10 00:05:10 +0100

    Setup devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
+  before_validation :default_params
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integar: true, greater_than_0r_qual_to: 0 }
@@ -9,4 +14,6 @@ class User < ApplicationRecord
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  root to: "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] 
   end
